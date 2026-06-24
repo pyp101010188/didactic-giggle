4fb352dfa12fbd0422362886c2538f8cedf55f12
GPTteam <108403455+pyp101010188@users.noreply.github.com>
2026-06-24 14:04:53 +0500

    Implement motor control functions in NAME.py
    Added functions for setting motor positions and converting degrees to motor turns.

--- NAME.py
+++ NAME.py
@@ -1,13 +1,16 @@
 import can
 import struct
+import time
 
-bus = can.interface.Bus(channel="can0", interface="socketcan")
+CAN_IFACE = "can1"   # у тебя физический CAN0 виден как can1
+GEAR_RATIO = 15.0
 
 NODE_IDS = [1, 2, 3, 4]
 
 IDLE = 1
-ENCODER_OFFSET = 7
 CLOSED_LOOP = 8
+
+bus = can.interface.Bus(channel=CAN_IFACE, interface="socketcan")
 
 def set_state(node_id, state):
     msg = can.Message(
@@ -16,20 +19,31 @@ def set_state(node_id, state):
         is_extended_id=False
     )
     bus.send(msg)
-    print("sent node:", node_id, "state:", state)
 
-# стоп всех
-for node in NODE_IDS:
-    set_state(node, IDLE)
+def set_pos(node_id, motor_turns):
+    msg = can.Message(
+        arbitration_id=(node_id << 5) | 0x0C,
+        data=struct.pack("<fhh", motor_turns, 0, 0),
+        is_extended_id=False
+    )
+    bus.send(msg)
 
-# калибровка энкодеров по очереди
-for node in NODE_IDS:
-    print("Калибрую encoder offset node", node)
-    set_state(node, ENCODER_OFFSET)
-    input("Когда мотор остановится, нажми Enter...")
+def deg_to_motor_turns(deg):
+    return deg / 360.0 * GEAR_RATIO
 
-# включить closed loop
+# Включить closed loop
 for node in NODE_IDS:
     set_state(node, CLOSED_LOOP)
+    print("CLOSED_LOOP:", node)
+
+time.sleep(1)
+
+# Повернуть все моторы на 25 градусов выхода редуктора
+angle_deg = 25
+turns = deg_to_motor_turns(angle_deg)
+
+for node in NODE_IDS:
+    set_pos(node, turns)
+    print("MOVE:", node, angle_deg, "deg =", turns, "motor turns")
 
 print("Готово")
